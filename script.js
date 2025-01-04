403a155ceb740c968a81baf8db3bb7c40fd1409f
waaltrick <waltrickcomv@gmail.com>
2025-01-04 11:54:08 -0300

    🎉 initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // se tiver light mode
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
+  }
+  if (html.classList.contains("light")) {
+    // se tiver sem light mode
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+}
